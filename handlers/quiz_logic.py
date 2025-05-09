2b6950a9026da274e0646c8f4a7cb2577c0b7750
moosahu <moosahu@hotmail.com>
2025-05-09 05:10:54 +0300

    Final Critical Fix: Remove unexpected 'wrong_answers' param from quiz_logic for stats logging

--- handlers/quiz_logic.py
+++ handlers/quiz_logic.py
@@ -478,7 +478,6 @@ class QuizLogic:
                     quiz_type=self.quiz_type,
                     total_questions=self.total_questions,
                     score=self.score,
-                    wrong_answers=self.total_questions - self.score - sum(1 for a in self.answers if a.get("chosen_option_id") is None and a.get("time_taken") == self.question_time_limit), # Approximation of wrong
                     skipped_answers=sum(1 for a in self.answers if a.get("chosen_option_id") is None and a.get("time_taken") == self.question_time_limit), # Skipped due to timeout
                     score_percentage=percentage,
                     time_taken_seconds=sum(a.get("time_taken", 0) for a in self.answers if a.get("time_taken", 0) > 0), # Sum of time per question
